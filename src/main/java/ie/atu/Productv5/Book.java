e3711f9744d3f28f5b702e46adb32427f35372fa
rory619 <G00422319@atu.ie>
2024-01-31 18:10:58 +0000

    committing files, polymorphism

---
+++ src/main/java/ie/atu/Productv5/Book.java
@@ -0,0 +1,27 @@
+package ie.atu.Productv5;
+
+public class Book extends Product {
+
+    private String author;
+
+    public Book() {
+        super();
+        author = "";
+        count++;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return super.toString() + " by " + author;
+    }
+}
